3c7a6a1a60510c458f81de37bc7dbe6c72a05b65
sauter001 <lsj457@naver.com>
2025-12-23 21:02:40 +0900

    feat: add calculating final value

--- src/main/java/store/domain/order/PaymentAmount.java
+++ src/main/java/store/domain/order/PaymentAmount.java
@@ -3,5 +3,7 @@ package store.domain.order;
 public record PaymentAmount(int totalAmount,
                             int promotionDiscount,
                             int membershipDiscount) {
-
+    public int calculateFinalAmount() {
+        return totalAmount - promotionDiscount - membershipDiscount;
+    }
 }
