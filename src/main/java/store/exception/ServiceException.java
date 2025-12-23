a699ac8513da45c37921910e702af073879186f3
sauter001 <lsj457@naver.com>
2025-12-23 22:40:24 +0900

    docs: check implemented function

--- src/main/java/store/exception/ServiceException.java
+++ src/main/java/store/exception/ServiceException.java
@@ -1,7 +1,9 @@
 package store.exception;
 
 public class ServiceException extends IllegalArgumentException {
+    public static final String ERROR_PREFIX = "[ERROR] ";
+
     public ServiceException(String message) {
-        super(message);
+        super(ERROR_PREFIX + message);
     }
 }
